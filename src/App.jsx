f658ee34a7c66c0676b3bfa5b82df8eae06d789f
gagan-1307 <149751804+gagan-1307@users.noreply.github.com>
2025-07-15 18:27:42 +0530

    Added speed-insights

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,9 @@ import Navbar from './Components/Navbar/Navbar';
 import Work from './Components/Work/Work';
 import Education from './Components/Education/Education';
 import Experience from './Components/Experience/Experience';
-import { Analytics } from "@vercel/analytics/next"
+import { Analytics } from "@vercel/analytics/react";
 import Blur from './Components/Blur';
+import { SpeedInsights } from "@vercel/speed-insights/react"
 import './index.css'
 
 function App() {
@@ -17,6 +18,8 @@ function App() {
         <Blur position={{top:'35%',left:'20%'}} size={{width:'30%',height:'40%'}}/>
         <div className="absolute inset-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:14px_24px] [mask-image:radial-gradient(ellipse_60%_50%_at_50%_0%,#000_70%,transparent_100%)]"></div>
         <div className='relative'>
+          <Analytics />
+          <SpeedInsights />
           <Navbar/>
           <About/>
           <Skills/>
@@ -25,7 +28,6 @@ function App() {
           <Work/>
           <Contact/>
           <Footer/>
-          <Analytics />
         </div>
       </div>
     </>
